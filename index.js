2dc5e1a2ed0e72d86000beaccddd6089e09b991c
mylkner <mylofaulkner27@gmail.com>
2024-04-02 04:23:17 +0200

    added try catch to mongoose connection

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+try {
+    mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+} catch (err) {
+    console.log("Mongo connection failed");
+}
 
 const createAndSaveUrl = async (url) => {
     try {
